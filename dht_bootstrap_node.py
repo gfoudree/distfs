9e684f961a786be5f51d4a128e691f698ec2f01e
Joel May <joel.may@live.com>
2018-12-10 15:12:56 -0600

    Added arguments

--- dht_bootstrap_node.py
+++ dht_bootstrap_node.py
@@ -3,10 +3,10 @@ import asyncio
 
 from kademlia.network import Server
 
-def launch_bootstrap():
+def launch_bootstrap(listenPort: int = 8468):
     
     server = Server()
-    server.listen(8468)
+    server.listen(listenPort)
     
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
